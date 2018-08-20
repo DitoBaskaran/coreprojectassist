b5e07c768a723a7e2ad8255bb814f22e187df689
DitoBaskaran <32388767+DitoBaskaran@users.noreply.github.com>
2018-08-20 14:41:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -37,6 +37,279 @@ if($message['type']=='text')
 						);
 				
 	}
+	
+	if($pesan_datang=='Caption' || $pesan_datang=='6')
+	{
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array (
+  'type' => 'template',
+  'altText' => 'Caption',
+  'template' => 
+  array (
+    'type' => 'buttons',
+    'thumbnailImageUrl' => 'https://1.bp.blogspot.com/-Ac7CLfJQm-s/W3Lzo84TkhI/AAAAAAAABL4/1TB46mZ2Hqo4Fzcy6vrTlTKcuXXdBviagCLcBGAs/s320/CoreProject.jpg',
+    'imageAspectRatio' => 'rectangle',
+    'imageSize' => 'cover',
+    'imageBackgroundColor' => '#FFFFFF',
+    'title' => 'Caption',
+    'text' => 'Silahkan pilih.',
+    'defaultAction' => 
+    array (
+      'type' => 'message',
+      'label' => 'Caption 1',
+      'text' => 'Caption 1',
+    ),
+    'actions' => 
+    array (
+      0 => 
+      array (
+        'type' => 'message',
+        'label' => 'Caption 1',
+        'text' => 'Caption 1',
+      ),
+      1 => 
+      array (
+        'type' => 'message',
+        'label' => 'Caption 2',
+        'text' => 'Caption 2',
+      ),
+    ),
+  ),
+)
+							)
+						);
+				
+	}
+	
+	if($pesan_datang=='Logo') {
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array (
+                                      'type' => 'image',
+                                      'originalContentUrl' => 'https://huskiest-takeoff.000webhostapp.com/CoreProject.jpg',
+                                      'previewImageUrl' => 'https://huskiest-takeoff.000webhostapp.com/CoreProject.jpg',
+                                    )
+							)
+						);
+				
+	}
+	
+	if($pesan_datang=='5' || $pesan_datang=='Admin') {
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array (
+  'type' => 'template',
+  'altText' => 'Admin CoreProject',
+  'template' => 
+  array (
+    'type' => 'carousel',
+    'columns' => 
+    array (
+      0 => 
+      array (
+        'thumbnailImageUrl' => 'https://3.bp.blogspot.com/-Ik5kF4Wg-S8/W3L9AyroiqI/AAAAAAAABMc/XtVi56F5O4kX4XOVRLDdb5omP6KgsQClwCLcBGAs/s1600/37044385_422047604981813_183265215138234368_n.jpg',
+        'imageBackgroundColor' => '#FFFFFF',
+        'title' => 'Owner',
+        'text' => 'DitoBaskaran',
+        'defaultAction' => 
+        array (
+          'type' => 'uri',
+          'label' => 'Buka',
+          'uri' => 'http://line.me/ti/p/~anonymousind',
+        ),
+        'actions' => 
+        array (
+        0 => 
+          array (
+           'type' => 'uri',
+          'label' => 'Buka',
+          'uri' => 'http://line.me/ti/p/~anonymousind',
+          ),
+        ),
+      ),
+      1 => 
+      array (
+        'thumbnailImageUrl' => 'https://instagram.fcgk1-1.fna.fbcdn.net/vp/7e6de955871d7bfdd73c41d84b945495/5BFBCB68/t51.2885-15/e35/25016744_2019657204925710_834670805999681536_n.jpg',
+        'imageBackgroundColor' => '#FFFFFF',
+        'title' => 'Admin 1',
+        'text' => 'Navyra',
+        'defaultAction' => 
+        array (
+          'type' => 'uri',
+          'label' => 'Buka',
+          'uri' => 'http://line.me/ti/p/~navyraflp_',
+        ),
+        'actions' => 
+        array (
+        0 => 
+          array (
+           'type' => 'uri',
+          'label' => 'Buka',
+          'uri' => 'http://line.me/ti/p/~navyraflp_',
+          ),
+        ),
+      ),
+      2 => 
+      array (
+        'thumbnailImageUrl' => 'https://huskiest-takeoff.000webhostapp.com/CoreProject.jpg',
+        'imageBackgroundColor' => '#FFFFFF',
+        'title' => 'Admin 2',
+        'text' => '-',
+        'defaultAction' => 
+        array (
+          'type' => 'message',
+          'label' => 'Buka',
+          'text' => 'Admin 2',
+        ),
+        'actions' => 
+        array (
+          0 => 
+          array (
+            'type' => 'message',
+            'label' => 'Buka',
+            'text' => 'Admin 2',
+          ),
+        ),
+      ),
+      3 => 
+      array (
+        'thumbnailImageUrl' => 'https://huskiest-takeoff.000webhostapp.com/CoreProject.jpg',
+        'imageBackgroundColor' => '#FFFFFF',
+        'title' => 'Admin 3',
+        'text' => '-',
+        'defaultAction' => 
+        array (
+          'type' => 'message',
+          'label' => 'Buka',
+          'text' => 'Admin 3',
+        ),
+        'actions' => 
+        array (
+          0 => 
+          array (
+            'type' => 'message',
+            'label' => 'Buka',
+            'text' => 'Admin 3',
+          ),
+        ),
+      ),
+    ),
+    'imageAspectRatio' => 'rectangle',
+    'imageSize' => 'cover',
+  ),
+)
+							)
+						);
+				
+	}
+	
+	if($pesan_datang=='Owner' || $pesan_datang=='Own') {
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array (
+  'type' => 'template',
+  'altText' => 'Owner CorePoject',
+  'template' => 
+  array (
+    'type' => 'buttons',
+    'thumbnailImageUrl' => 'https://instagram.fcgk1-1.fna.fbcdn.net/vp/fb611678be9b8dd1da7e3245d05d8682/5BF44FA0/t51.2885-15/e35/s480x480/37044385_422047604981813_183265215138234368_n.jpg',
+    'imageAspectRatio' => 'rectangle',
+    'imageSize' => 'cover',
+    'imageBackgroundColor' => '#FFFFFF',
+    'title' => 'Owner',
+    'text' => 'Dia adalah owner CoreProject!',
+    'defaultAction' => 
+    array (
+      'type' => 'uri',
+      'label' => 'Buka',
+      'uri' => 'https://www.instagram.com/_ditobskrn_',
+    ),
+    'actions' => 
+    array (
+      0 => 
+      array (
+        'type' => 'uri',
+        'label' => 'Buka',
+        'uri' => 'https://www.instagram.com/_ditobskrn_',
+      ),
+    ),
+  ),
+)
+							)
+						);
+				
+	}
+	
+	if($pesan_datang=='Help') {
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array (
+  'type' => 'template',
+  'altText' => 'Help',
+  'template' => 
+  array (
+    'type' => 'buttons',
+    'thumbnailImageUrl' => 'https://huskiest-takeoff.000webhostapp.com/CoreProject.jpg',
+    'imageAspectRatio' => 'rectangle',
+    'imageSize' => 'cover',
+    'imageBackgroundColor' => '#FFFFFF',
+    'title' => 'Help',
+    'text' => 'Silahkan pilih.',
+    'defaultAction' => 
+    array (
+      'type' => 'message',
+      'label' => 'Owner',
+      'text' => 'https://www.instagram.com/_ditobskrn_',
+    ),
+    'actions' => 
+    array (
+      0 => 
+      array (
+        'type' => 'message',
+        'label' => 'Owner',
+        'text' => 'Owner',
+      ),
+      1 => 
+      array (
+        'type' => 'message',
+        'label' => 'Official Account',
+        'text' => 'Official Account',
+      ),
+      2 => 
+      array (
+        'type' => 'message',
+        'label' => 'Link',
+        'text' => 'Link',
+      ),
+      3 => 
+      array (
+        'type' => 'message',
+        'label' => 'Keywords',
+        'text' => 'Key',
+      ),
+    ),
+  ),
+)
+							)
+						);
+				
+	}
 
 }
  
